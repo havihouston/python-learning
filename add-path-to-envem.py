d7d64f12727e2a3fbbfc190469424534cbc3fbcf
havihouston <havihouston@outlook.com>
2018-06-06 17:28:36 +0800

    Add files via upload

---
+++ add-path-to-envem.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 06 17:26:23 2018
+
+@author: havi
+"""
+
+import sys
+new_path = 'C:/Users/havi/.spyder'
+if new_path not in sys.path:
+    sys.path.append(new_path)
